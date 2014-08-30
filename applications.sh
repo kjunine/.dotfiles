096dffde1123a54d69e6e7b0b0fdd1aea9aaf6d1
Daniel Ku <kjunine@gmail.com>
2014-08-30 22:12:01 +0900

    Adds more vagrant plugins.

--- applications.sh
+++ applications.sh
@@ -4,4 +4,6 @@ brew bundle applications
 
 # vagrant
 vagrant plugin install sahara
+vagrant plugin install vagrant-berkshelf --plugin-version 2.0.1
+vagrant plugin install vagrant-omnibus
 vagrant plugin install vagrant-aws
