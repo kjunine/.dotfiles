0732ea5b1d9ebf464d975d64749156735fc5d4ea
Daniel Ku <kjunine@gmail.com>
2014-10-18 03:32:45 +0900

    Changes order in C1-development.sh.

--- C1-development.sh
+++ C1-development.sh
@@ -9,18 +9,17 @@ brew install docker boot2docker
 brew linkapps
 
 echo "\n# pyenv\neval \"\$(pyenv init -)\"" >> ~/.zshrc
+echo "\n# rbenv\neval \"\$(rbenv init -)\"" >> ~/.zshrc
+echo "\n# nvm\nsource \$(brew --prefix nvm)/nvm.sh" >> ~/.zshrc
 source ~/.zshrc
+
 pyenv install 3.4.1
 pyenv install 2.7.8
 pyenv global 3.4.1
 
-echo "\n# rbenv\neval \"\$(rbenv init -)\"" >> ~/.zshrc
-source ~/.zshrc
 rbenv install 2.1.3
 rbenv global 2.1.3
 
-echo "\n# nvm\nsource \$(brew --prefix nvm)/nvm.sh" >> ~/.zshrc
-source ~/.zshrc
 nvm install 0.10
 nvm alias default 0.10
 
