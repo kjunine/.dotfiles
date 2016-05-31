920ce2813cd93edb9e66e306d4705d4c4c2a3823
Daniel Kyojun Ku <d@nielku.com>
2016-05-31 22:11:45 +0900

    Update npm packages.

--- C4-nodejs.sh
+++ C4-nodejs.sh
@@ -6,6 +6,7 @@ nvm install 4.4
 nvm alias default 4.4
 nvm ls
 
+npm install -g yo
 npm install -g bower
 npm install -g gulp-cli
 npm install -g grunt-cli
@@ -13,11 +14,13 @@ npm install -g locally
 npm install -g coffee-script
 npm install -g typescript
 npm install -g typings
-npm install -g mocha
-npm install -g node-inspector
+npm install -g eslint
 npm install -g jshint
-npm install -g csslint
 npm install -g coffeelint
-npm install -g gitbook
-npm install -g hexo-cli
-npm install -g docpad
+npm install -g tslint
+npm install -g csslint
+npm install -g sass-lint
+
+# npm install -g gitbook
+# npm install -g hexo-cli
+# npm install -g docpad
