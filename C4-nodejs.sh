28d1d127ba632c4491354323f9d33207ee9f158e
Daniel Kyojun Ku <d@nielku.com>
2016-06-27 18:53:44 +0900

    Add http-server and remove locally in node dependencies

--- C4-nodejs.sh
+++ C4-nodejs.sh
@@ -12,7 +12,6 @@ npm install -g yo
 npm install -g bower
 npm install -g gulp-cli
 npm install -g grunt-cli
-npm install -g locally
 npm install -g coffee-script
 npm install -g typescript
 npm install -g typings
@@ -22,6 +21,7 @@ npm install -g coffeelint
 npm install -g tslint
 npm install -g csslint
 npm install -g sass-lint
+npm install -g http-server
 
 # npm install -g gitbook
 # npm install -g hexo-cli
