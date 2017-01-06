550db4ee14383841864d8676c53d2d001ba2f5f1
Daniel Kyojun Ku <d@nielku.com>
2017-01-06 23:07:42 +0900

    Rename files and add cask packages

---
+++ D2-cask.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+
+brew cask install tuxera-ntfs
+brew cask install google-chrome
+brew cask install dropbox
+brew cask install google-drive
+brew cask install 1password
+brew cask install iterm2
+brew cask install alfred
+brew cask install bartender
+brew cask install istat-menus
+
+brew cask install evernote
+brew cask install skype
+brew cask install sourcetree
+brew cask install dash
+brew cask install mplayerx
+brew cask install battle-net
+
+brew linkapps
---
+++ D3-cask.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+brew cask install webstorm
+brew cask install atom
+brew cask install sublime-text
+brew cask install scrivener
+brew cask install marked
+brew cask install paw
+brew cask install mongochef
+brew cask install sequel-pro
+brew cask install docker
+brew cask install kitematic
+
+brew cask install android-studio
+brew cask install unity
+brew cask install virtualbox
+
+brew linkapps
