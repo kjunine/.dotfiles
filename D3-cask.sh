a580c2b2e2c207039141876579fa7414f01fda68
Daniel Kyojun Ku <d@nielku.com>
2018-03-13 16:04:49 +0900

    Update

--- D3-cask.sh
+++ D3-cask.sh
@@ -5,10 +5,11 @@ brew cask install sublime-text
 brew cask install scrivener
 brew cask install marked
 brew cask install paw
-brew cask install studio-3t
-brew cask install sequel-pro
 
 brew cask install sketch
+brew cask install studio-3t
+brew cask install sequel-pro
+brew cask install teamviewer
 brew cask install docker
 brew cask install virtualbox
 brew cask install android-studio
