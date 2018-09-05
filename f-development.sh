5e85ae9b3b6e85b44f580c5d312714a41d9bad25
Daniel Kyojun Ku <d@nielku.com>
2018-09-05 17:28:09 +0900

    Update

--- f-development.sh
+++ f-development.sh
@@ -12,12 +12,11 @@ brew install elasticsearch
 brew install passenger
 brew install nginx --with-passenger
 brew install sqlite
-brew install mysql@5.7
+brew install mysql
 brew install postgresql
 brew install imagemagick
 
 echo "# pyenv\neval \"\$(pyenv init -)\"\n" >> ~/.zshrc
 echo "# rbenv\neval \"\$(rbenv init -)\"\n" >> ~/.zshrc
 echo "# nvm\nexport NVM_DIR=\"\$HOME/.nvm\"\n. \"/usr/local/opt/nvm/nvm.sh\"\n" >> ~/.zshrc
-echo "# mysql@5.7\nexport PATH=\"/usr/local/opt/mysql@5.7/bin:\$PATH\"\n" >> ~/.zshrc
 source ~/.zshrc
