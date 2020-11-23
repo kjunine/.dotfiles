401a78ea6b3fc30d784758bacd20384ed24483af
Daniel Kyojun Ku <d@nielku.com>
2020-11-23 22:10:39 +0900

    Update

--- k-mas.sh
+++ k-mas.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env zsh
 
 mas install 497799835   # Xcode
+mas install 640199958   # Developer
+mas install 1496833156  # Playgrounds
 mas install 682658836   # GarageBand
-mas install 490152466   # iBooks Author
 mas install 408981434   # iMovie
 mas install 424389933   # Final Cut Pro
 mas install 634148309   # Logic Pro X
@@ -11,37 +12,44 @@ mas install 424390742   # Compressor
 mas install 409183694   # Keynote
 mas install 409201541   # Pages
 mas install 409203825   # Numbers
+
+mas install 462058435   # Microsoft Excel
+mas install 462054704   # Microsoft Word
+mas install 462062816   # Microsoft PowerPoint
+mas install 985367838   # Microsoft Outlook
+mas install 823766827   # OneDrive
+mas install 1274495053  # Microsoft To Do
+mas install 1295203466  # Microsoft Remote Desktop
+
 mas install 1333542190  # 1Password
-mas install 1091189122  # Bear
-mas install 904280696   # Things 3
-mas install 918858936   # Airmail 3
-mas install 975937182   # Fantastical 2
+mas install 904280696   # Things
+mas install 918858936   # Airmail
+mas install 975937182   # Fantastical
 mas install 869223134   # KakaoTalk
 mas install 419330170   # Moom
 mas install 955848755   # Theine
-mas install 540348655   # Monosnap
-mas install 880001334   # Reeder
-mas install 413965349   # Soulver
 mas install 1055511498  # Day One
+mas install 413965349   # Soulver
+
 mas install 1289583905  # Pixelmator Pro
-mas install 407963104   # Pixelmator
 mas install 777874532   # Cinemagraph Pro
-mas install 568020055   # Scapple
-mas install 715768417   # Microsoft Remote Desktop
 mas install 1055273043  # PDF Expert
 mas install 492835684   # Bandizip X
 mas install 639764244   # Xee³
 mas install 461788075   # Movist
-mas install 672206759   # Disk Diag
-mas install 525971999   # Display Desktop
 mas install 403388562   # Transmit
-mas install 414209656   # Better Rename 9
-mas install 489880259   # ScreenShot PSD
-mas install 597419160   # Sleep Pillow
-mas install 766939888   # 1Keyboard
-mas install 866773894   # Quiver
-mas install 406056744   # Evernote
-mas install 867299399   # OmniFocus
-mas install 623795237   # Ulysses
+mas install 568020055   # Scapple
 
-sudo xcodebuild -license accept
+# mas install 672206759   # Disk Diag
+# mas install 414209656   # Better Rename 9
+# mas install 489880259   # ScreenShot PSD
+# mas install 597419160   # Sleep Pillow
+# mas install 766939888   # 1Keyboard
+# mas install 540348655   # Monosnap
+# mas install 880001334   # Reeder
+
+# mas install 1091189122  # Bear
+# mas install 866773894   # Quiver
+# mas install 406056744   # Evernote
+# mas install 867299399   # OmniFocus
+# mas install 623795237   # Ulysses
