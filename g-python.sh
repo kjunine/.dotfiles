d08a84540051e68a1197ccf22bd035ff346715dd
Daniel Kyojun Ku <d@nielku.com>
2022-11-24 20:30:31 +0900

    Update

--- g-python.sh
+++ g-python.sh
@@ -7,6 +7,7 @@ pyenv versions
 
 pyenv exec pip install --upgrade pip
 pyenv exec pip install pylint
+pyenv exec pip install ipython
 pyenv exec pip install jupyterlab
 # pyenv exec pip install awscli
 # pyenv exec pip install boto
